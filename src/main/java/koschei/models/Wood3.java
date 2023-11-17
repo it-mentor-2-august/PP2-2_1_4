9a6eb72095aace89ec1beb54c88e3b4294e0c9e4
andrey <you@example.com>
2023-11-18 00:46:06 +0300

    Complete task 2.1.4

---
+++ src/main/java/koschei/models/Wood3.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Wood3 {
+
+
+    private Rabbit4 rabbit4;
+
+    @Override
+    public String toString() {
+        return ", на дереве заяц " + rabbit4.toString();
+    }
+
+    @Autowired
+    public void setRabbit4(Rabbit4 rabbit4) {
+        this.rabbit4 = rabbit4;
+    }
+}
